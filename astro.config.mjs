08b623a42570ef3192047821cfc3fad76be2e718
Arjan <ahoekdev@gmail.com>
2026-05-01 07:52:46 +0200

    update sanity config

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,5 +10,12 @@ import sanity from "@sanity/astro";
 // https://astro.build/config
 export default defineConfig({
   adapter: netlify(),
-  integrations: [react(), sanity()],
+  integrations: [
+    react(),
+    sanity({
+      projectId: "677exyo6",
+      dataset: "production",
+      useCdn: false, // for static builds
+    }),
+  ],
 });
